49f1cc9ac1407ad8f8cf7d2779ccdc95f115daa2
pavel <pashashabashoff@gmail.com>
2020-04-20 19:23:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/qeref/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.qeref
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
+class MainClass{
+    
+}
+class Game{
+    
+}
+class Grafics (context:Context, attributeSet: AttributeSet, context{
+
+}
